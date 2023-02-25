5c40a678fcedf535c896a32428c3acc279c23c49
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-02-25 21:19:27 +0100

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// Tworzenie i renderowanie znacznika
+const list = document.querySelector('.gallery')
+const markup = galleryItems
+     .map(image => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img class="gallery__image" 
+                    src="${image.preview}"
+                    data-original="${image.original}"
+                    alt="${image.description}"/>
+            </a>
+        </div>`)
+    .join("");
+
+list.insertAdjacentHTML('afterbegin', markup);
+
+// Implementacja delegowania i otrzymanie większego obrazu
+
+document.querySelector('.gallery').onclick = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    } 
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.original}" width="800" height="600">`);
+
+    instance.show()
+
+    const closeImage = (event) => {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    }
+    document.addEventListener('keydown', closeImage);
+    }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector('.gallery')
+const markup = galleryItems
+     .map(image => 
+        `<a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" 
+                src="${image.preview}"
+                data-original="${image.original}"
+                alt="${image.description}"/>
+         </a>
+        `)
+    .join("");
+
+list.insertAdjacentHTML('afterbegin', markup);
